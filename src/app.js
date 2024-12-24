f32c72743459353827210c0e47d23445f32530ce
Inamul <inamulhussain@colanonline.com>
2024-12-24 21:16:52 +0530

    json token added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookieParser";
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 
 const app = express();
@@ -11,21 +12,22 @@ app.use(
   })
 );
 
+//common middleware
 app.use(
   express.json({
     limit: "16kb",
   })
 );
-
 app.use(
   express.urlencoded({
     limit: "16kb",
     extended: true,
   })
 );
-
 app.use(express.static("public"));
+app.use(cookieParser());
 
+//routes
 app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export { app };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrpyt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,42 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  //next is  a middleware  .next is  a way to pass on your request from one middlware to the next middleware whereever it need to go
+
+  if (!this.modified("password")) return next();
+  this.password = bcrpyt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrpyt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  //short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  //short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
