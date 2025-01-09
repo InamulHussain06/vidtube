e6e73fccb8ad0a56878a7c189bdcb5720eb3ac8c
inamul <h.n.inamulh@gmail.com>
2025-01-10 00:03:37 +0530

    error middleWare added

--- src/middleware/error.middlewares.js
+++ src/middleware/error.middlewares.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
-import { ApiError } from "../utils/ApiError";
+import { ApiError } from "../utils/ApiError.js";
 
 const errorHandler = (err, req, res, next) => {
   const error = err;
   
+  if(!(error instanceof ApiError)){
+      const statusCode=error.statusCode || error instanceof mongoose.Error ? 400 :500;
+
+      const message=error.message || 'Something went wrong'
+
+      error=new ApiError(statusCode,message,error?.error || [],err.stack)
+
+
+  }
+
+
+  const response={
+    ...error,
+    message:error.message,
+    // ...(process.env.)
+  }
+
+
 };
 
 export { errorHandler };
